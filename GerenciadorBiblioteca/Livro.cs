443479979fa4bb55788b21e10a217e71d9055dd7
HannaNascente <hanna.nascente22@gmail.com>
2024-08-11 22:51:38 -0300

    Add Gerenciador de Biblioteca

---
+++ GerenciadorBiblioteca/Livro.cs
@@ -0,0 +1,99 @@
+﻿using System.Text.Json;
+
+namespace GerenciadorBiblioteca
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string ISBN { get; set; }
+        public int AnoPublicacao { get; set; }
+
+        public static void Cadastrar(List<Livro> livros)
+        {
+            var livro = new Livro();
+            
+            Console.Write("Informe o título do livro: ");
+            livro.Titulo = Console.ReadLine()!;
+           
+            Console.Write("Informe o id do livro: ");
+            livro.Id = int.Parse(Console.ReadLine()!);
+            
+            Console.Write("Informe o autor do livro: ");
+            livro.Autor = Console.ReadLine()!;
+           
+            Console.Write("Informe o ISBN do livro: ");
+            livro.ISBN = Console.ReadLine()!;
+            
+            Console.Write("Informe o ano de publicação do livro: ");
+            livro.AnoPublicacao = int.Parse(Console.ReadLine()!);
+
+            livros.Add(livro);
+
+            Console.WriteLine("\nLivro cadastrado com sucesso!");
+            Console.WriteLine("\nPressione qualquer tecla para voltar para o menu.");
+            Console.ReadKey();
+        }
+
+        public static void Listar(List<Livro> livros)
+        {
+            Console.WriteLine("\nLivros cadastrados:\n");
+
+            foreach (var livro in livros)
+            {
+                Console.WriteLine(JsonSerializer.Serialize(livro));
+            }
+
+            Console.WriteLine("\nPressione qualquer tecla para voltar para o menu.");
+            Console.ReadKey();
+        }
+
+        public static void ConsultarPorId(List<Livro> livros)
+        {
+            Console.Write("\nDigite o id do livro: ");
+            var idInformado = Console.ReadLine()!;
+
+            var livro = BuscarLivro(livros, idInformado);
+
+            if (livro == null)
+            {
+                Console.WriteLine("\nLivro não encontrado.");
+            }
+            else
+            {
+                Console.WriteLine(JsonSerializer.Serialize(livro));
+            }
+
+            Console.WriteLine("\nPressione qualquer tecla para voltar para o menu.");
+            Console.ReadKey();
+        }
+
+        public static void RemoverPorId(List<Livro> livros)
+        {
+            Console.Write("\nDigite o id do livro que deseja remover: ");
+            var idInformado = Console.ReadLine()!;
+
+            var livro = BuscarLivro(livros, idInformado);
+
+            if (livro == null)
+            {
+                Console.WriteLine("\nLivro não encontrado.");
+            }
+            else
+            {
+                livros.Remove(livro);
+
+                Console.WriteLine("\nLivro removido com sucesso!");
+            }
+            
+            Console.WriteLine("\nPressione qualquer tecla para voltar para o menu.");
+            Console.ReadKey();
+        }
+
+        private static Livro BuscarLivro(List<Livro> livros, string idInformado)
+        {
+            return livros.Find(l => l.Id == int.Parse(idInformado));
+        }
+    }
+}
---
+++ GerenciadorBiblioteca/Program.cs
@@ -0,0 +1,41 @@
+﻿using GerenciadorBiblioteca;
+
+var execute = true;
+var livros = new List<Livro>();
+
+while (execute)
+{
+    Console.Clear();
+    Console.WriteLine("*** Gerenciador de Biblioteca ***");
+    Console.WriteLine("1.Cadastrar um livro");
+    Console.WriteLine("2.Consultar todos os livros");
+    Console.WriteLine("3.Consultar um livro");
+    Console.WriteLine("4.Remover um livro");
+    Console.WriteLine("5.Sair");
+
+    Console.Write("\nEscolha uma das opções: ");
+    var opcaoEscolhida = Console.ReadLine()!;
+
+    switch (opcaoEscolhida)
+    {
+        case "1":
+            Livro.Cadastrar(livros);
+            break;
+        case "2":
+            Livro.Listar(livros);
+            break;
+        case "3":
+            Livro.ConsultarPorId(livros);
+            break;
+        case "4":
+            Livro.RemoverPorId(livros);
+            break;
+        case "5":
+            execute = false;
+            break;
+        default:
+            Console.WriteLine("Opção inválida. Pressione qualquer tecla para voltar para o menu.");
+            Console.ReadKey();
+            break;
+    }
+}
